d59b47451c3020bcd37968beda4fba93309227d5
Samantha Vuignier <cartoon_air@MacBook-Air.fios-router.home>
2021-11-07 10:52:38 -0500

    County results added to txt file

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -101,10 +101,11 @@ with open(file_to_save, "w") as txt_file:
 
         # 6d: Print the county results to the terminal.
         county_results = (
-            f"{county_name}: {vote_percentage:.1f}% ({votes:,})")
+            f"{county_name}: {vote_percentage:.1f}% ({votes:,})\n")
         print(county_results)
 
         # 6e: Save the county votes to a text file.
+        txt_file.write(county_results)
 
         # 6f: Write an if statement to determine the winning county and get its vote count.
         if (votes > county_vote_count) and (vote_percentage > winning_percentage):
@@ -119,9 +120,8 @@ with open(file_to_save, "w") as txt_file:
         f"-------------------------\n")
     print(winning_county_summary)
 
-
     # 8: Save the county with the largest turnout to a text file.
-
+    txt_file.write(winning_county_summary)
 
     # Save the final candidate vote count to the text file.
     for candidate_name in candidate_votes:
